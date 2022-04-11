c026ef133812341c7729497396223c89f2656065
Matthew Becker <becker@tamu.edu>
2022-04-11 15:41:48 -0500

    Update button pin

--- Frisbee/scripts/ButtonTest/ButtonTest.ino
+++ Frisbee/scripts/ButtonTest/ButtonTest.ino
@@ -23,7 +23,7 @@
 */
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
+const int buttonPin = 3;     // the number of the pushbutton pin
 const int ledPin = LED_BUILTIN;      // the number of the LED pin
 
 // variables will change:
@@ -39,11 +39,13 @@ void setup() {
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
+  
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
+      
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
