2fcf1dc3a7bb457483d9658c8fe8013f1fc54a46
Matthew Becker <becker@tamu.edu>
2022-04-10 20:36:47 -0500

    Update serial assignment

--- Frisbee/scripts/BluetoothAcknowledgeTest/BluetoothAcknowledgeTest.ino
+++ Frisbee/scripts/BluetoothAcknowledgeTest/BluetoothAcknowledgeTest.ino
@@ -1,25 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial2.begin(9600);
+  Serial5.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial2.print("ComCheck\n");
-  String received = Serial2.readString();
+  Serial5.print("ComCheck\n");
+  String received = Serial5.readString();
   Serial.println(received);
 
   if (received.startsWith("ComEstablished"))
   {
     Serial.println("Sending data");
-    Serial2.print("SendingData\n");
+    Serial5.print("SendingData\n");
     for (int i = 0; i < 300; i++)
     {
-      Serial2.print(i);
-      Serial2.print(',');
+      Serial5.print(i);
+      Serial5.print(',');
     }
-    Serial2.print("\nEndTransmission\n");
+    Serial5.print("\nEndTransmission\n");
   }
   else
   {
