5f4bedddc2f2b5e78b8ed196f59d0fcd0c56e796
Matthew Becker <becker@tamu.edu>
2022-04-13 13:25:23 -0500

    Add preliminary calibration values

--- Frisbee/main/config.h
+++ Frisbee/main/config.h
@@ -1,3 +1,11 @@
 #define DEBUG_SERIAL true
 
 #define GPS_FIX_REQUIRED false
+
+// MPU calibration
+#define MPU_OFFSET_AX 0.06
+#define MPU_OFFSET_AY -0.01
+#define MPU_OFFSET_AZ 11.22
+#define MPU_OFFSET_GX -0.09
+#define MPU_OFFSET_GY 0.03
+#define MPU_OFFSET_GZ 0.00
