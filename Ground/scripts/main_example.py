7169a0d4de36cd40792ec4a08f47d0a42e8ccead
Matthew Becker <becker@tamu.edu>
2022-04-24 09:46:28 -0500

    Restore main_example.py test script

---
+++ Ground/scripts/main_example.py
@@ -0,0 +1,17 @@
+from bluetooth_receiver import BluetoothReceiver
+import time
+
+interface = BluetoothReceiver()
+
+while True:
+    data = interface.collectData()
+
+    if data is not None:
+        # if data is not none, you have received some data
+
+        for List in data:
+            print(List)
+
+        break
+
+    time.sleep(1)
